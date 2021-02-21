4f52c35b86207955b06f2c745b7a16bdb4e4e0bf
Philip Heggelund <59456945+DuckScapePhilip@users.noreply.github.com>
2021-02-21 16:14:00 -0300

    Added the express server .

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TrawlerAPI
-ffffffffffffffffffff
+The trawler API is a temprorary REST API that can be used to utilize the trawler library over REST. It has a single command called
+`csv`.
---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const trawler = require('@gftc/trawler')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/profile', upload.array(), function (req, res, next) {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/csv', upload.array(), async function (req, res, next) {
+
+  console.log(req.body);
+
+  const [
+    bdhXml,
+    epcClassXml,
+    locationXml,
+    objectEventXmlList,
+    transformationEventXmlList,
+    aggregationEventXmlList
+  ] = await Promise.all([
+    trawler.createBusinessDocumentHeaderXml(req.body.businessHeaderCsv),
+    trawler.createEpcClassXml(req.body.epcClassCsv),
+    trawler.createLocationHeaderXml(req.body.locationCsv),
+    trawler.createObjectEventXml(req.body.objectEventCsv),
+    trawler.createTransformationEventXml(req.body.transformationEventCsv),
+    trawler.createAggregationEventXml(req.body.aggregationEventCsv)
+  ])
+  
+  // Combine the generator's result to create the final EPCIS document 
+  const result = trawler.createTrawlerXml({
+    bdhXml,
+    epcClassXml,
+    locationXml,
+    xmlList: [
+      ...objectEventXmlList,
+      ...transformationEventXmlList,
+      ...aggregationEventXmlList
+    ]
+  })
+
+  console.log(result);
+
+  res.send(result);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
